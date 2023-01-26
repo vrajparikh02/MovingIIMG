6af3435d7a88c34d3ed68d03ae12d3dceade8da0
vrajparikh02 <85411665+vrajparikh02@users.noreply.github.com>
2023-01-26 18:42:04 -0500

    Add files via upload

---
+++ C102/C102_assets-main/C102.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+from_dir='C:/Users/Vraj/Desktop/PYTHONBY/C102/C102_assets-main/'
+to_dir='C:/Users/Vraj/Desktop/PYTHONBY/C102/C102_assets-main/images'
+list_of_files=os.listdir(from_dir)
+for file_name in list_of_files:
+    name,extension=os.path.splitext(file_name)
+    if extension=='':
+        continue
+    if extension in ['.gif','.png','.jpg','.jpeg','.jfif']:
+        path1=from_dir+'/'+file_name
+        path2=to_dir+'/'+'Image_Files'
+        path3=to_dir+'/'+'Image_Files'+'/'+file_name
+        if os.path.exists(path2):
+            print("moving "+file_name+"...")
+            shutil.move(path1,path3)
+        else:
+            os.makedirs(path2)
+            print("movining"+file_name+"...")
+            shutil.move(path1,path3)
